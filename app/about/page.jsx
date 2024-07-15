1af287149b1414fdac0ceb64717d5b0062820dfd
Hunter Knott <hunterknott00@gmail.com>
2024-07-14 22:00:55 -0600

    Starting on about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,21 +1,24 @@
+import NavBar from '../NavBar';
+
 export default function Page() {
     return (
-        <main className="flex min-h-screen flex-col gap-6 items-center justify-center bg-gray-100 py-24">
-            <div className="flex flex-row gap-20 max-w-[800px] mt-[-96px] w-full mx-auto px-8 text-center justify-center items-center">
-                <h1 className="font-bold">
-                    What is ClrDoc?
-                </h1>
-                <p>
-                    ClrDoc is a platform for patients to be connected with healthcare providers and know their starting service prices upfront. It&#39;s a great resource for people and families to plan ahead and stay on top of all their healthcare needs.
-                </p>
-            </div>
-            <div className="flex flex-row gap-20 max-w-[800px] mt-[-96px] w-full h-screen mx-auto px-8 text-center justify-center items-center">
-                <p>
-                    ClrDoc is transparent and easy to use. Patients can ease of access to local providers in areas like dental and vision care.
-                </p>
-                <h1 className="font-bold">
-                    Why ClrDoc?
-                </h1>
+        <main className="flex min-h-screen flex-col gap-6 items-center bg-gray-100">
+            <div className="max-w-[800px] mt-[-196px] w-full h-screen px-8 mx-auto pb-0 text-center flex flex-col gap-24 justify-center items-center">
+                <h1 className="font-bold md:text-5xl sm:text-3xl text-2xl">About Us</h1>
+                <div className='flex flex-col md:flex-row gap-20'>
+                    <div className='flex flex-col gap-6'>
+                        <h2>Jaron Anderson</h2>
+                        <div className='container'>
+                            <img src="Images/JaronPicture.png" alt="Image" />
+                        </div>
+                    </div>
+                    <div className='flex flex-col gap-6'>
+                        <h2>Hunter Knott</h2>
+                        <div className='container'>
+                            <img src="Images/HunterPicture.png" alt="Images" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
     )
--- app/page.jsx
+++ app/page.jsx
@@ -13,9 +13,36 @@ export default function Home() {
                     <Link href={'./patient'}>
                         <button className="bg-blue-500 w-[200px] rounded-md font-medium text-gray-200 my-6 py-3 hover:bg-blue-600 transition duration-300">Search Providers</button>
                     </Link>
-                    <Link href={"Home"}>
-                        <button className="text-gray-800 font-bold transition-colors duration-150 hover:text-blue-500">Learn More &rarr;</button>
-                    </Link>
+                </div>
+            </div>
+            <div className="flex flex-row gap-12 p-8 md:w-[60%]">
+                <h1 className="font-bold md:text-5xl sm:text-3xl text-2xl">
+                        What is ClrDoc?
+                </h1>
+                <p>
+                    We connect with providers so you don&#39;t have to. ClrDoc offers clear, reliable information to eliminate the hassle of contacting multiple providers and comparing costs. With us, you can access transparent pricing, find local providers, and receive quality care easily.
+                </p>
+            </div>
+            <div className="flex flex-row gap-12 p-8 md:w-[60%]">
+                <p>
+                    ClrDoc simplifies your search for local doctors and provides clarity on pricing, especially for self-pay patients. We facilitate direct connections with providers, ensuring transparency for informed healthcare decisions. Our platform prioritizes access to quality care, offering a straightforward solution to find the right healthcare provider without financial uncertainties.
+                </p>
+                <h1 className="font-bold md:text-5xl sm:text-3xl text-2xl">
+                    Why ClrDoc?
+                </h1>
+            </div>
+            <div className='bg-gray-300 flex flex-col md:flex-row w-full px-8 py-20 mb-20 gap-20'>
+                <div>
+                    <span className="text-4xl font-bold">26 Million people</span>
+                    <p>in the United States were uninsured in 2022 according to a report by the Census Bureau.</p>
+                </div>
+                <div>
+                    <span className="text-4xl font-bold">73% of adults</span>
+                    <p>say they are worried about the cost of healthcare services.</p>
+                </div>
+                <div>
+                    <span className="text-4xl font-bold">61% of U.S. adults</span>
+                    <p>see reducing healthcare costs as a top priority as of 2022.</p>
                 </div>
             </div>
         </main>
