06c472dea42a3955b9f30d214f44339b412165a3
Hunter Knott <hunterknott00@gmail.com>
2024-07-15 19:33:22 -0600

    Sample about page

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,20 +4,21 @@ export default function Page() {
     return (
         <main className="flex min-h-screen flex-col gap-6 items-center bg-gray-100">
             <NavBar options={["App", "Providers", "Contact"]}/>
-            <div className="max-w-[800px] mt-[-196px] w-full h-screen px-8 pb-1 mx-auto text-center flex flex-col gap-0 justify-center items-center">
-                <h1 className="font-bold md:text-5xl sm:text-3xl text-2xl">About Us</h1>
+            <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col gap-6 justify-center items-center">
+                <h1 className="font-bold md:text-5xl sm:text-3xl text-2xl pb-12">About Us</h1>
                 <div className='flex flex-col md:flex-row gap-20'>
                     <div className='flex flex-col gap-6'>
-                        <h2>Jaron Anderson</h2>
+                        <h2 className="font-bold md:text-3xl sm:text-xl text-2xl">Jaron Anderson</h2>
                         <div className='container'>
-                            <img src="Images/JaronPicture.png" alt="Image" />
+                            <img src="Images/JaronPicture.png" alt="Image" className='mb-8 w-[400px]' />
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                         </div>
                     </div>
                     <div className='flex flex-col'>
-                        <h2>Hunter Knott</h2>
-                        <div className='container'>
-                            <img src="Images/HunterPicture.png" alt="Images" />
-                            <p></p>
+                        <h2 className="font-bold md:text-3xl sm:text-xl text-2xl">Hunter Knott</h2>
+                        <div className='container pl-6'>
+                            <img src="Images/HunterPicture.png" alt="Images" className='mb-8 w-[300px]' />
+                            <p>Hi there, I'm Hunter Knott. I'm currently studying Computer Science at Utah Valley University. I have experience in software development and data analytics in a manufacturing environment, along with business web development. I love what I do!</p>
                         </div>
                     </div>
                 </div>
@@ -26,4 +27,4 @@ export default function Page() {
     )
 }
 
-// Hi there, I'm Hunter Knott. I'm currently studying Computer Science at Utah Valley University. I have experience in software development and data analytics in a manufacturing environment, along with business web development. I love what I do!
+// 
--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
                 <h1 className="font-bold md:text-5xl sm:text-3xl text-2xl">A platform to find transparent healthcare pricing</h1>
                 <p className="font-bold">Patients have access to leading healthcare practitioners and their pricing. Providers have access to a wider group of patients.</p>
                 <div className="flex flex-row gap-8 justify-center items-center">
-                    <Link href={'./patient'}>
+                    <Link href={'./providers'}>
                         <button className="bg-blue-500 w-[200px] rounded-md font-medium text-gray-200 my-6 py-3 hover:bg-blue-600 transition duration-300">Search Providers</button>
                     </Link>
                 </div>
