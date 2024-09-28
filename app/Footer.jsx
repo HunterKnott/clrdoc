8bb5c9ce7513659289dc2e56971e674c74f7daef
Hunter Knott <hunterknott00@gmail.com>
2024-09-28 15:00:54 -0600

    Added matching custom logo to footer

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Image from 'next/image';
 
-export default function Footer() {
+export default function Footer({ background = '#0C0526', logoText = "ClrDoc", logoImage = "/Images/ClrDocIcon.png"}) {
     return (
-        <footer className='flex items-center justify-center w-full py-4 md:py-8' style={{backgroundColor: '#0C0526'}}>
+        <footer className='flex items-center justify-center w-full py-4 md:py-8 mt-4' style={{backgroundColor: background}}>
             <div className='flex flex-row gap-4 items-center'>
-                <h1 className="text-2xl md:text-4xl font-bold text-white">ClrDoc</h1>
-                <Image src="/Images/ClrDocIcon.png" alt="Icon" width={60} height={60} className="w-[30px] md:w-[60px]" />
+                <h1 className="text-2xl md:text-4xl font-bold text-white">{logoText}</h1>
+                <img
+                    src={logoImage}
+                    alt="Icon"
+                    width={60}
+                    height={60}
+                    className={logoImage === "/Images/ClrDocIcon.png" ? "w-[30px] md:w-[60px]" : "w-[180px]"}
+                />
             </div>
         </footer>
     )
