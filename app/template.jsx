98febe5eeedff723cded2e2eb64f832e7195296c
Hamnivore <samuelhwhitlock@gmail.com>
2024-09-30 21:33:43 -0600

    removed an unnecessary library

--- app/template.jsx
+++ app/template.jsx
@@ -1,14 +1,22 @@
 'use client';
-import { motion } from 'framer-motion';
+import React, { useState, useEffect } from 'react';
 
 export default function Template({ children }) {
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        setIsVisible(true);
+    }, []);
+
     return (
-        <motion.div
-            initial={{ y: 20, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ ease: 'easeInOut', duration: 0.5 }}
+        <div
+            style={{
+                opacity: isVisible ? 1 : 0,
+                transform: `translateY(${isVisible ? 0 : 20}px)`,
+                transition: 'opacity 0.5s ease-in-out, transform 0.5s ease-in-out',
+            }}
         >
             {children}
-        </motion.div>
+        </div>
     )
 }
