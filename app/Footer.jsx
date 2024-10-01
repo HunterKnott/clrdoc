2610ea70c7de4636cb9e23438d2930a4c1d9d735
Hamnivore <samuelhwhitlock@gmail.com>
2024-09-30 21:17:19 -0600

    footer is same as main branch now

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import Image from 'next/image';
 
-export default function Footer({ background = '#0C0526', logoText = "ClrDoc", logoImage = "/Images/ClrDocIcon.png"}) {
+export default function Footer() {
     return (
-        <footer className='flex items-center justify-center w-full py-4 md:py-8 mt-2' style={{backgroundColor: background}}>
+        <footer className='flex items-center justify-center w-full py-4 md:py-8' style={{backgroundColor: '#0C0526'}}>
             <div className='flex flex-row gap-4 items-center'>
-                <h1 className="text-2xl md:text-4xl font-bold text-white">{logoText}</h1>
-                <img
-                    src={logoImage}
-                    alt="Icon"
-                    width={60}
-                    height={60}
-                    className={logoImage === "/Images/ClrDocIcon.png" ? "w-[30px] md:w-[60px]" : "w-[180px]"}
-                />
+                <h1 className="text-2xl md:text-4xl font-bold text-white">ClrDoc</h1>
+                <img src="Images/ClrDocIcon.png" alt="Icon" className="w-[30px] md:w-[60px]" />
             </div>
         </footer>
     )
