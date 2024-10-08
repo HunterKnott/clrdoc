35c8c13baa1a338325695bb33aca90b11fa4b4a1
Hunter Knott <hunterknott00@gmail.com>
2024-10-08 14:23:34 -0600

    Removed some unneeded components

--- app/actions.ts
+++ app/actions.ts
@@ -5,35 +5,35 @@ import { createClient } from "@/utils/supabase/server";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
-export const signUpAction = async (formData: FormData) => {
-  const email = formData.get("email")?.toString();
-  const password = formData.get("password")?.toString();
-  const supabase = createClient();
-  const origin = headers().get("origin");
+// export const signUpAction = async (formData: FormData) => {
+//   const email = formData.get("email")?.toString();
+//   const password = formData.get("password")?.toString();
+//   const supabase = createClient();
+//   const origin = headers().get("origin");
 
-  if (!email || !password) {
-    return { error: "Email and password are required" };
-  }
+//   if (!email || !password) {
+//     return { error: "Email and password are required" };
+//   }
 
-  const { error } = await supabase.auth.signUp({
-    email,
-    password,
-    options: {
-      emailRedirectTo: `${origin}/auth/callback`,
-    },
-  });
+//   const { error } = await supabase.auth.signUp({
+//     email,
+//     password,
+//     options: {
+//       emailRedirectTo: `${origin}/auth/callback`,
+//     },
+//   });
 
-  if (error) {
-    console.error(error.code + " " + error.message);
-    return encodedRedirect("error", "/sign-up", error.message);
-  } else {
-    return encodedRedirect(
-      "success",
-      "/sign-up",
-      "Thanks for signing up! Please check your email for a verification link.",
-    );
-  }
-};
+//   if (error) {
+//     console.error(error.code + " " + error.message);
+//     return encodedRedirect("error", "/sign-up", error.message);
+//   } else {
+//     return encodedRedirect(
+//       "success",
+//       "/sign-up",
+//       "Thanks for signing up! Please check your email for a verification link.",
+//     );
+//   }
+// };
 
 // For keeneye as of now, credentials are doctor@email.com and Jzh8v_uODk6
 export const signInAction = async (formData: FormData) => {
